d9013867bb99915b3f6d55e82875a1747a5932ac
joaosamuelgomes <joaosamuelg001@gmail.com>
2022-06-24 10:10:22 -0300

    Atualizando router para adicionar a tela de subscribe

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { EventPlatform } from "./pages/EventPlatform";
+import { Subscribe } from "./pages/Subscribe";
 
 export function Router() {
 
     return(
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}/>
+            <Route path="/" element={<Subscribe />}/>
             <Route path="/event" element={<EventPlatform/>} />
             <Route path="/event/lesson/:slug" element={<EventPlatform/>} />
         </Routes>
